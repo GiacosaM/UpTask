884cd9c2d0da39622d9254f61d0fec1433c3bfa1
Martin <martingiacosa@gmail.com>
2022-04-26 12:09:12 -0300

    sol. error

--- classes/Email.php
+++ classes/Email.php
@@ -39,7 +39,7 @@ class Email {
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has Creado tu cuenta en 
         UpTask, solo debes confirmarla en el siguiente enlace </P>";
-        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/confirmar?token=".$this->token."'>Confirmar Cuenta</a> </p>";
         $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje </p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
