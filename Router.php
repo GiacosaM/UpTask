49bec8df8b12e0da4f2eb28d6a31278010071c34
Martin <martingiacosa@gmail.com>
2022-04-26 07:44:04 -0300

    error sol

--- Router.php
+++ Router.php
@@ -19,14 +19,14 @@ class Router
 
     public function comprobarRutas()
     {
-       $currentUrl = $_SERVER['PATH_INFO']?? '/';  
+       //$currentUrl = $_SERVER['PATH_INFO']?? '/';  
        // $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         
-      /*  if ($_SERVER['PATH_INFO']) {
+      if ($_SERVER['PATH_INFO']) {
         $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
      } else {
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-     } */
+     } 
 
        $method = $_SERVER['REQUEST_METHOD'];
 
