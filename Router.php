e193105403ebb3686cb674e9244035162231597b
Martin <martingiacosa@gmail.com>
2022-04-27 06:37:49 -0300

    act

--- Router.php
+++ Router.php
@@ -17,7 +17,8 @@ class Router
         $this->postRoutes[$url] = $fn;
     }
 
-    /* public function comprobarRutas() {
+    public function comprobarRutas() {
+        
         $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
         $method = $_SERVER['REQUEST_METHOD'];
     
@@ -37,31 +38,9 @@ class Router
         } else {
             echo "Página No Encontrada o Ruta no válida";
         }
-    } */
+    } 
 
-    public function comprobarRutas()
-    {
-
-        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        $method = $_SERVER['REQUEST_METHOD'];
-
-        if ($method === 'GET') {
-            $fn = $this->getRoutes[$currentUrl] ?? null;
-        } else {
-            $fn = $this->postRoutes[$currentUrl] ?? null;
-        }
-
-
-        if ( $fn ) {
-            // Call user fn va a llamar una función cuando no sabemos cual sera
-            call_user_func($fn, $this); // This es para pasar argumentos
-        } else {
-            echo "Página No Encontrada o Ruta no válida";
-            //http_response_code(404);
-            //include('views/error.php'); // provide your own HTML for the error page  
-            
-        }
-    }
+  
 
     public function render($view, $datos = [])
     {
